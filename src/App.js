ee5af72e1c1029f8f35c0cdb07b6f49223637d80
noemi3a <nhenrique@3anet.com.br>
2024-02-04 19:04:52 -0300

    projeto inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Banner from './Componentes/Banner/Banner';
+import Formulario from './Componentes/Formulario/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+    </div>
+  )
+}
+
+
+
+export default App;
+      
---
+++ src/Componentes/Formulario/Formulario.js
@@ -0,0 +1,17 @@
+import CampoTexto from '../CampoTexto/CampoTexto'
+import './Formulario.css'
+
+
+const Formulario = () => {
+    return (
+        <section>
+            <form>
+                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
