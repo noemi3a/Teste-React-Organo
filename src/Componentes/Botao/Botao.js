30a1d5a0e8e4dfd43bcda8832610c2b3adb2a790
noemi3a <nhenrique@3anet.com.br>
2024-02-05 00:13:02 -0300

    Lista Suspensa e botão

---
+++ src/Componentes/Botao/Botao.js
@@ -0,0 +1,14 @@
+import './Botao.css'
+
+const Botao = (props) => {
+    return (
+    <button className='botao'>
+         {props.texto}
+
+        </button>)
+}
+
+export default Botao
+
+
+
--- src/Componentes/Formulario/Formulario.js
+++ src/Componentes/Formulario/Formulario.js
@@ -1,14 +1,29 @@
+import Botao from '../Botao/Botao' 
 import CampoTexto from '../CampoTexto/CampoTexto'
+import ListaSuspensa from '../ListaSuspensa/ListaSuspensa'
+
 import './Formulario.css'
 
 
-const Formulario = () => {
+
+const Formulario = (props) => {
+
+    const planos =[
+      '3A Digital',
+      'Frontend na 3A',
+      'Backend na 3A'
+    ]
     return (
-        <section>
+        <section className="formulario">
             <form>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+            <h2>Preencha os dados para criar o card do curso:</h2>
+            
+                <CampoTexto label="Curso" placeholder="Digite o curso"/>
+                <CampoTexto label="Plano" placeholder="Digite o plano de Estudos"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+
+                <ListaSuspensa label="Plano"itens={planos} />
+                <Botao texto="Criar Card"/> 
             </form>
         </section>
     )
